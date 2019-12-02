41b95681fe4e9d8944c142ae31327aaef04c24aa
Vahik P <vaheek.papazian@hotmail.com>
2019-12-02 15:19:47 +0100

    v1.0

--- exe_3.2.4.sol
+++ exe_3.2.4.sol
@@ -3,104 +3,76 @@ import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.so
 
 contract CagnotteFestival {
   using SafeMath for uint;
-  address[] private organisateurs;
-  string[] private sponsors;
-  uint public placesRestantes = 100000;
-  uint private _depensesTotales;
-  uint private _depenseLimite = 20000;
-  uint public dateFestival;
-  uint _dateLiquidation = dateFestival + 2 weeks;
+  mapping (address => uint) organisateurs; // Mapping des organisateurs et leurs parts
+    string[] sponsors; // Liste des sponsors
+    uint constant LIMITE = 100; // Limite des sponsors à ajouter
+    uint dateFestival;
+    uint dateLiquidation;
+    uint constant seuilDepense = 100 ether; // Seuil dépense par jour
+    mapping (uint => uint) depenses; // Jour => valeur
+    uint partsTotal = 100;
 
-  mapping (address => uint) private pourcentage;
-  mapping (address => bool) private festivaliers;
-  mapping (uint => uint) private depensesJour;
 
-  constructor() public {
-    pourcentage[msg.sender] = 100;
-    organisateur.push(msg.sender)
-    dateFestival = block.timestamp;
+  constructor(uint date) public {
+   require(date >= block.timestamp, "La date renseignée est fausse !");
+    organisateurs[msg.sender] = partsTotal;
+    dateFestival = date;
+    dateLiquidation = dateFestival +2 weeks;
   }
 
   function estOrga(address orga) public view returns (bool){
           require(orga != address(0));
-          if (pourcentage[orga] > 0 ){
-            return true;
-          } else {
-            return false;
-          }
+         return (organisateurs[orga] > 0);
   }
 
   function transfererOrga(address orga, uint parts) public {
       require(orga != address(0));
-      require(pourcentage[msg.sender] >= parts, "demander une part moins importante!");
+      require(organisateurs[msg.sender] >= parts, "demander une part moins importante!");
       require(!estOrga(orga), "Vous n"'etes pas organisteur");
-      pourcentage[msg.sender] = pourcentage[msg.sender].sub(parts);
-      pourcentage[orga] = parts;
-      organisateurs.push(orga);
-  }
-
-  function acheterTicket() public payable {
-    require(msg.value>= 500 finney,"Place à 0.5 Ethers");
-    require(placesRestantes>0,"Plus de places !");
-    festivaliers[msg.sender]=true;
-    placesRestantes = placesRestantes.sub(1);
+      organisateurs[msg.sender] = organisateurs[msg.sender].sub(parts);
+      organisateurs[orga] = parts;
+      //organisateurs.push(orga);
   }
 
 
-  function payer(address  destinataire, uint montant) payable public {
-    require(destinatire != address(0));
-    require(estOrga(msg.sender));
-    require(destinataire != address(0));
-    require(montant > 0);
-    require(address(this).balance >= montant, "Fonds insufissante");
-    require(_comptabiliserDepense(montant), "Limite journalière atteinte");
-    if (((block.timestamp)/ 1 days) == depensesJour[0]){
-      depensesJour[0] = ((block.timestamp)/ 1 days);
-    }
-    depensesJour[(block.timestamp)/ 1 days] = depensesJour[(block.timestamp)/ 1 days].add(montant);
-    _depensesTotales = _depensesTotales.add(montant);
-    destinataire.transfer(montant);
+  function jour(uint t) internal pure returns (uint) {
+       // Indice du jour du moment t
+       return SafeMath.div(t, 1 days);
+   }
+
+  function controleDepense(uint valeur) internal view returns (bool) {
+    // vérifier la dépense du jour
+    return SafeMath.add(depenses[jour(block.timestamp)], valeur) <= seuilDepense;
   }
 
-  function _comptabiliserDepense(uint montant) private view returns (bool){
-        if (depensesJour[(block.timestamp)/ 1 days].add(montant) > _depenseLimite){
-              return false;
-      } else {
-              return true;
-          }
-  }
 
-  function voirDepensesTotales() public view returns (uint){
-        require(estOrga(msg.sender), "Vous n'êtes pas organisateur");
-        return _depensesTotales;
-  }
 
   function sponsoriser(string memory nom) public payable {
         require(msg.sender != address(0));
-        require(msg.value >= 30 ether && sposors.length <= LIMITE, "Minimum amount to be sponsor is 30 ETH and sponsors limit is reached");
+        require(msg.value >= 30 ether && sponsors.length <= LIMITE, "Minimum amount to be sponsor is 30 ETH and sponsors limit is reached");
         sponsors.push(nom);
   }
 
-  function retraitOrganisateurs(){
-     require(block.timestamp >= dateLiquidation);
-     if (organisateurs.length == 1) {
-        selfdestruct(msg.sender);
-     } else {
-       msg.sender.transfer(address(this).balance/pourcentage[msg.sender]);
-     }
-
-     for (uint i = 0; i < organisateurs.length; i++) {
-       if(organisateurs[i] == msg.sender) {
-          delete organisateurs[i];
-          }
-     }
-
+  function retraitOrganisateurs() public{
+    // vérifier que la date de liquidation est atteinte
+    require(block.timestamp >= dateLiquidation, "La date de liquidation n'est pas dépassée !");
+    // to avoid re-entrancy
+    uint toTransfer = (address(this).balance).mul(organisateurs[msg.sender].div(partsTotal));
+    // mettre à jour les parts
+    partsTotal = partsTotal.sub(organisateurs[msg.sender]);
+    assert(partsTotal <= 100);
+    // supprimer l'organisateur
+    delete organisateurs[msg.sender];
+    // transferer le montant équivalent à ses parts au msg.sender
+    msg.sender.transfer(toTransfer);
+    // vérifier si y'a plus de parts, kill le contrat
+    if (partsTotal == 0) {
+      selfdestruct(msg.sender);
+    }
   }
 
 
   function () external payable{
    }
-
-
 
   }
