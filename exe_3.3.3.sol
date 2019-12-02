336572f312585bfe36f4e2b5c6a3e90b84e64ef2
Vahik P <vaheek.papazian@hotmail.com>
2019-12-02 10:40:02 +0100

    v1.0

--- exe_3.3.3.sol
+++ exe_3.3.3.sol
@@ -1,16 +1,19 @@
 pragma solidity ^0.5.0;
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Pulsation {
   uint public battement;
- constructor() public {
-  uint battement = 0;
- }
+  string private message;
 
- function ajouterBattement() {
-    battement.add(1);
+ constructor(string memory _message) public {
+   battement = 0;
+   message = _message;
   }
-}
+
+ function ajouterBattement() public returns(string memory) {
+    battement++;
+    return  message;
+    }
+  }
 
 
 contract Pendule {
@@ -18,22 +21,15 @@ contract Pendule {
  string[] public balancier;
  Pulsation tac;
  Pulsation tic;
- bool public ping;
 
 
- function ajouterTacTic(Pulsation _tic, Pulsation _tac)public{
+ function ajouterTacTic(Pulsation _tic, Pulsation _tac) public{
    tic = _tic;
    tac = _tac;
  }
 
  function mouvementsBalancier() public {
-    if(ping = true) {
     balancier.push(tic.ajouterBattement());
-    ping = false;
-  } else if (ping = false) {
     balancier.push(tac.ajouterBattement());
-    ping = true;
- }
-}
-
+  }
 }
