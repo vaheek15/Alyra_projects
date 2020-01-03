02d297375808c1ef53fca532068addd7bb11e4ec
Vahik P <vaheek.papazian@hotmail.com>
2020-01-03 16:34:37 +0100

    0.99

--- exe_8.1.1.sol
+++ exe_8.1.1.sol
@@ -3,22 +3,32 @@ pragma solidity 0.5.0;
 contract CanalDePaiement {
 
 
-enum EtatCanal {VIDE, ACTIF, ENCOURSFERMETURE, FERME}
-address partieA;
-address partieB;
-uint montant;
-EtatCanal etat;
-uint blocFermeture;
-uint dernierNonce;
-uint equilibreA;
-uint equilibreB;
+  enum EtatCanal {VIDE, ACTIF, ENCOURSFERMETURE, FERME}
+  address payable partieA;
+  address payable partieB;
+  uint montant;
+  EtatCanal etat;
+  uint blocFermeture;
+  uint dernierNonce;
+  uint equilibreA;
+  uint equilibreB;
 
 
-constructor(uint _amount, address payable _partie1, address payable _partie2) public payable {
-  montant = _amount;
-  partieA = _partie1;
-  partieB = _partie2;
-  EtatCanal.ACTIF;
-  }
+  constructor(uint _amount, address payable _partie1, address payable _partie2) public payable {
+    require(_amount > 0, "le Canal ne peut pas etre déficitaire!");
+    _amount = equilbreA + equilibreB;
+    partieA = _partie1;
+    partieB = _partie2;
+    EtatCanal.ACTIF;
+    }
+
+
+
+
+
+
+
+
+
 
 }
